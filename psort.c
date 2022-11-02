6882e3a56b30bfe97e718e47ab53d03103e6b2a7
Jason Lin <jasonlin8812@gmail.com>
2022-11-02 17:47:57 -0500

    Added tester file and got some input

--- psort.c
+++ psort.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
-int main() {
+int main(int argc, char** argv) {
     // parse input - use threading
     FILE *fp;
-    char buffer[100];
+    //char buffer[100];
+    int filelen;
 
-    fp = fopen("file", "rb");
-    fread(buffer, strlen(c)+1, 1, fp);
-    fclose(fp);
     
+    // fp = fopen(argv[1], "rb");
+    int fd = open(argv[1], O_RDWR);         
+    struct stat statbuf;
+    int err = fstat(fd, &statbuf);
+    if (err < 0) {
+        printf("\n\"%s \" could not open\n", argv[1]);
+        exit(2);
+    }
+    int *ptr = mmap(0, statbuf.st_size, PROT_READ | PROT_EXEC, MAP_SHARED, fd, 0 );
+    
+    for(int i = 0; i<statbuf.st_size; i++) {
+        printf("%d", ptr[i]);
+    }
+    close(fd);
+    // fclose(fp);
+    // ssize_t n = write(1, ptr, statbuf.st_size);    
 
     // sort     
     
