e6f3b096ae8bfcee01504124d312d93bc5a437e7
Jason Lin <linj@rockhopper-06.cs.wisc.edu>
2022-11-02 16:21:01 -0500

    Made psort.c

---
+++ psort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main() {
+    // parse input - use threading
+    FILE *fp;
+    char buffer[100];
+
+    fp = fopen("file", "rb");
+    fread(buffer, strlen(c)+1, 1, fp);
+    fclose(fp);
+    
+
+    // sort     
+    
+    
+    // send to output
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
