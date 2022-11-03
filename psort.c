523aa816585dd51424287cfac5b8908f2d05678b
Avi Sharma <sharma@royal-18.cs.wisc.edu>
2022-11-03 16:15:28 -0500

    Read in file using mmap

--- psort.c
+++ psort.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
-#include <sys/mman.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <stdio.h>
+#include <pthread.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
+struct rec {
+    int key[1];
+    int value[24]; // 96 bytes
+};
 
 int main(int argc, char** argv) {
     // parse input - use threading
-    FILE *fp;
+    // FILE *fp;
     //char buffer[100];
     int filelen;
 
@@ -16,19 +23,36 @@ int main(int argc, char** argv) {
     // fp = fopen(argv[1], "rb");
     int fd = open(argv[1], O_RDWR);         
     struct stat statbuf;
-    int err = fstat(fd, &statbuf);
+    int err = stat(argv[1], &statbuf);
     if (err < 0) {
         printf("\n\"%s \" could not open\n", argv[1]);
         exit(2);
     }
-    int *ptr = mmap(0, statbuf.st_size, PROT_READ | PROT_EXEC, MAP_SHARED, fd, 0 );
-    
-    for(int i = 0; i<statbuf.st_size; i++) {
-        printf("%d", ptr[i]);
+
+    filelen = statbuf.st_size;
+
+    int entry = filelen % 100;
+
+    // printf("%ld\n", entry * sizeof(struct rec));
+
+    //converted to char pointer
+    //made map private to avoid modifications from other thread.
+    char *ptr = (char *) mmap(0, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0 );
+
+    //create an array of structs
+    struct rec *records = malloc(entry * sizeof(struct rec));
+
+    int j = 0;
+
+    //copy key and value into struct
+    for(int i = 0; i<statbuf.st_size; i = i + 100) {
+        memcpy(records[j].key, ptr + i, 4);
+        memcpy(records[j].value, ptr + i, 96);
+        j++;
+        // printf("%s\n", ptr + i);
     }
+
     close(fd);
-    // fclose(fp);
-    // ssize_t n = write(1, ptr, statbuf.st_size);    
 
     // sort     
     
